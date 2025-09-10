93aaed7bbafd967d71d8872e08d14c76fd430b79
Alex Toon <altoon@IU.EDU>
2025-09-09 22:12:14 -0400

    week 2 submission #1
    first attempt of both exercises for week 2

--- apputil.py
+++ apputil.py
@@ -3,11 +3,23 @@ import numpy as np
 
 # update/add code below ...
 
-def ways(n):
-    return None
+def ways(n: int):
+    # returns the number of ways to make n cents with pennies and nickels
+    if n < 0:
+        return 0
+    return n // 5 + 1
+
+
+
 
 def lowest_score(names, scores):
-    return None
+    #returns JUST the name of the person with the lowest score
+    min_index = np.argmin(scores)
+    return names[min_index]
+
+
 
 def sort_names(names, scores):
-    return None
+    # returns the names in the order their scores in descending order
+    sorted_indices = np.argsort(scores) [::-1]  # for sorting in descending order
+    return names[sorted_indices]
